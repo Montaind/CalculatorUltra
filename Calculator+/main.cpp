4b9e7c617b8485a278a95c3658e1578fc470daa9
Богдан Грибачев <banlypro10@gmail.com>
2024-03-10 20:05:50 +0300

    not working will do it later

--- Calculator+/main.cpp
+++ Calculator+/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 int main() {
-	int count = 0;
+	string actions = {"0"};
 	string str;
 	getline(cin, str);
 	bool first = false;
@@ -18,63 +18,76 @@ int main() {
 	double result;
 	double buffer;
 	char action_2 = 0;
+	int count = 0;
 	for (int i = 0; i < str.size(); i++) {
 		if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+			if (str[i] == '+') {
+				actions[i] = '+';
+			}
+			else if (str[i] == '-') {
+				actions[i] =  '-';
+			}
+			else if (str[i] == '*') {
+				actions[i] = '*';
+			}
+			else if (str[i] == '/') {
+				actions[i] = '/';
+			}
 			count++;
 		}
 	}
 	for (int j = 0; j < count; j++) {
 		for (int i = 0; i < str.size(); i++) {
-            if (str[i] >= '0' && str[i] <= '9') {
-                if (first_end) {
-                    second = true;
-                    second_n = str[i] - 48;
-                    second_buffer = (second_buffer * 10) + second_n;
-                }
-                else {
-                    first = true;
-                    first_n = str[i] - 48;
-                    first_buffer = (first_buffer * 10) + first_n;
-                }
+			if (str[i] >= '0' && str[i] <= '9') {
+				if (first_end) {
+					second = true;
+					second_n = str[i] - 48;
+					second_buffer = (second_buffer * 10) + second_n;
+				}
+				else {
+					first = true;
+					first_n = str[i] - 48;
+					first_buffer = (first_buffer * 10) + first_n;
+				}
 
-            }
-            else
-                if (str[i] == '+') {
-                    first_end = true;
-                    action = '+';
-                }
-                else
-                    if (str[i] == '-') {
-                        first_end = true;
-                        action = '-';
-                    }
-                    else
-                        if (str[i] == '*') {
-                            first_end = true;
-                            action = '*';
-                        }
-                        else if (str[i] == '/') {
-                            first_end = true;
-                            action = '/';
-                        }
-        }
-        if (first && action && second) {
-            if (action == '+') {
-                result = first_buffer + second_buffer;
-            }
-            else if (action == '-') {
-                result = first_buffer - second_buffer;
-            }
-            else if (action == '*') {
-                result = first_buffer * second_buffer;
-            }
-            else if (action == '/') {
-                result = first_buffer / second_buffer;
-            }
-        }
-        first_buffer = second_buffer;
-        second = false;
-        second_end = false;
-        second_buffer = 0;
+			}
+			else
+				if (str[i] == '+') {
+					first_end = true;
+					action = '+';
+				}
+				else
+					if (str[i] == '-') {
+						first_end = true;
+						action = '-';
+					}
+					else
+						if (str[i] == '*') {
+							first_end = true;
+							action = '*';
+						}
+						else if (str[i] == '/') {
+							first_end = true;
+							action = '/';
+						}
+		}
+		if (first && action && second) {
+			if (action == '+') {
+				result = first_buffer + second_buffer;
+			}
+			else if (action == '-') {
+				result = first_buffer - second_buffer;
+			}
+			else if (action == '*') {
+				result = first_buffer * second_buffer;
+			}
+			else if (action == '/') {
+				result = first_buffer / second_buffer;
+			}
+		}
+		first_buffer = second_buffer;
+		second = false;
+		second_end = false;
+		second_buffer = 0;
 	}
 }
