0b00ccd2d296a40ba514d44f130159bda2acd5c7
Богдан Грибачев <banlypro10@gmail.com>
2024-03-11 23:07:43 +0300

    Not do anything

--- Calculator+/main.cpp
+++ Calculator+/main.cpp
@@ -1,93 +1,79 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 int main() {
-	string actions = {"0"};
+	string actions;
 	string str;
+	vector<int> positions;
 	getline(cin, str);
 	bool first = false;
 	bool first_end = false;
 	bool second = false;
 	bool second_end = false;
 	char action = 0;
+	double all_buffer;
 	double first_n;
 	double second_n;
 	double first_buffer = 0;
 	double second_buffer = 0;
 	double result;
 	double buffer;
+	int a;
+	int b;
 	char action_2 = 0;
 	int count = 0;
+	string v;
+	for (char c : str) if (c != ' ') v += c;
+	str = v;
+	cout << str << endl;
 	for (int i = 0; i < str.size(); i++) {
 		if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
-			if (str[i] == '+') {
-				actions[i] = '+';
+			{
+				if (str[i] == '+') {
+					actions.push_back('+');
+					positions.push_back(i);
+				}
+				else if (str[i] == '-') {
+					actions.push_back('-');
+					positions.push_back(i);
+				}
+				else if (str[i] == '*') {
+					actions.push_back('*');
+					positions.push_back(i);
+				}
+				else if (str[i] == '/') {
+					actions.push_back('/');
+					positions.push_back(i);
+				}
+				count++;
 			}
-			else if (str[i] == '-') {
-				actions[i] =  '-';
-			}
-			else if (str[i] == '*') {
-				actions[i] = '*';
-			}
-			else if (str[i] == '/') {
-				actions[i] = '/';
-			}
-			count++;
 		}
 	}
-	for (int j = 0; j < count; j++) {
-		for (int i = 0; i < str.size(); i++) {
-			if (str[i] >= '0' && str[i] <= '9') {
-				if (first_end) {
-					second = true;
-					second_n = str[i] - 48;
-					second_buffer = (second_buffer * 10) + second_n;
-				}
-				else {
-					first = true;
-					first_n = str[i] - 48;
-					first_buffer = (first_buffer * 10) + first_n;
-				}
+	for (int i = 0; i < count; i++) {
+		if (actions[i] == '*' || actions[i] == '/') {
+			int a = positions[i];
+			int b = positions[i - 1];
+			actions.erase(i);
+			positions.erase(positions.begin()+(i-1));
+			break;
+		}
+	}
 
-			}
-			else
-				if (str[i] == '+') {
-					first_end = true;
-					action = '+';
-				}
-				else
-					if (str[i] == '-') {
-						first_end = true;
-						action = '-';
-					}
-					else
-						if (str[i] == '*') {
-							first_end = true;
-							action = '*';
-						}
-						else if (str[i] == '/') {
-							first_end = true;
-							action = '/';
-						}
-		}
-		if (first && action && second) {
-			if (action == '+') {
-				result = first_buffer + second_buffer;
-			}
-			else if (action == '-') {
-				result = first_buffer - second_buffer;
-			}
-			else if (action == '*') {
-				result = first_buffer * second_buffer;
-			}
-			else if (action == '/') {
-				result = first_buffer / second_buffer;
-			}
-		}
-		first_buffer = second_buffer;
-		second = false;
-		second_end = false;
-		second_buffer = 0;
+
+
+
+
+
+
+
+
+	for (int i = 0; i < count; i++) {
+		cout << actions[i];
+	}
+	cout << "\n";
+	for (int i = 0; i < positions.size(); i++) {
+		cout << positions[i] << " ";
 	}
 }
